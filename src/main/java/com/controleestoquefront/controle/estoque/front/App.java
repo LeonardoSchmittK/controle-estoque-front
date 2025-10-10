72fa76456d09d3a2b7a7e1a6cec6d8d1a1fccb43
Leoschmittk <Leoeyeschmittk@gmail.com>
2025-10-09 21:34:58 -0300

    feat: first commit hello world

---
+++ src/main/java/com/controleestoquefront/controle/estoque/front/App.java
@@ -0,0 +1,28 @@
+package com.controleestoquefront.controle.estoque.front;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        Button btn = new Button("Hello JavaFX with Maven!");
+        btn.setOnAction(e -> System.out.println("Hello world!"));
+
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+
+        Scene scene = new Scene(root, 400, 300);
+        primaryStage.setTitle("JavaFX Maven Demo");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
