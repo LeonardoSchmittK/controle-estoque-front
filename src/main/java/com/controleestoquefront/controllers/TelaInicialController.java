17b86c3db61c730df787d517fdc6e5439067e6f4
Mateus Almeida <165097236+mtgjbr@users.noreply.github.com>
2025-11-10 22:47:29 -0300

    feat:adicionando a tela inicial e os controllers da tela incial.

---
+++ src/main/java/com/controleestoquefront/controllers/TelaInicialController.java
@@ -0,0 +1,13 @@
+package com.controleestoquefront.controllers;
+import javafx.fxml.FXML;
+public class TelaInicialController {
+    private javafx.scene.layout.AnchorPane panePrincipal;
+
+   
+    @FXML
+    private void botaoClicado() {
+        System.out.println("Botão foi clicado!");
+    }
+
+
+}
