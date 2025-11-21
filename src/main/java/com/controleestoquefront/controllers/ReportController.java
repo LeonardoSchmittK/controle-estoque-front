c61246745551ce19aaba9b722e880b1e38ff9b99
Mateus Almeida <165097236+mtgjbr@users.noreply.github.com>
2025-11-21 20:42:01 -0300

    Feat: add Report Controller

---
+++ src/main/java/com/controleestoquefront/controllers/ReportController.java
@@ -0,0 +1,18 @@
+
+package com.controleestoquefront.controllers;
+public class ReportController {
+
+    public ReportController() {
+       
+    }
+
+  
+    public void handleGenerateReport() {
+
+    }
+
+ 
+    public void handleDisplayReport() {
+        
+    }
+}
